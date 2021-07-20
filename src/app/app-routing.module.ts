a0d1abb339e2d66630528a75fb8d9051398af562
Michele Wiesner <michywiesner@gmail.com>
2021-07-20 16:35:33 -0300

    base href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  },
-  { path: '**', redirectTo: ''}
+  }
 ];
 
 @NgModule({
